afec29101d21153601687e9ff441e144e1cfd933
oleg <olegpustovalov220@gmail.com>
2023-06-04 13:30:35 +0300

    Window application to download videos from Youtube.
    Оконное приложение для скачивания видео с Youtube.

---
+++ youtube_r.py
@@ -0,0 +1,50 @@
+import os
+import subprocess
+from tkinter import *
+from pytube import YouTube
+
+root = Tk()
+root.geometry('600x120')
+root.title("Скачать видео с YouTube")
+root["bg"] = "#3c3f41"
+link = StringVar()
+entry_text = "Введите ссылку"
+link_enter = Entry(root, width=44, textvariable=link)
+link_enter.place(x=20, y=20)
+
+
+def on_entry_click(event):
+    if link_enter.get() == entry_text:
+        link_enter.delete(0, END)
+        link_enter.configure(fg="black")
+
+
+def on_entry_leave(event):
+    if link_enter.get() == '':
+        link_enter.insert(0, entry_text)
+        link_enter.configure(fg="gray")
+
+
+link_enter.insert(0, entry_text)
+link_enter.configure(fg="gray")
+link_enter.bind('<FocusIn>', on_entry_click)
+link_enter.bind('<FocusOut>', on_entry_leave)
+
+
+def YouTubeDownloader():
+    if link.get() != "" and link.get() != entry_text:
+        url = YouTube(str(link.get()))
+        video = url.streams.get_highest_resolution()
+        video.download()
+        file_path = os.path.join(os.getcwd()) #, video.default_filename)
+        Label(root, text=f'Видео скачено{file_path}', font='arial 10', bg="#D55B06", fg="white").place(x=20, y=60)
+        subprocess.call(["xdg-open", file_path])
+    else:
+        Label(root, text='Cсылка пуста!', font='arial 10', bg=root["bg"], fg="red").place(x=20, y=60)
+
+
+text_button = 'arial 12 bold'
+clear_button = Button(root, text='Очистить', font=text_button, bg='white', fg="black", command=lambda: link.set('')).place(x=380, y=20)
+Button(root, text='Скачать', font=text_button, bg='white', fg="black", command=YouTubeDownloader).place(x=486, y=20)
+
+root.mainloop()
